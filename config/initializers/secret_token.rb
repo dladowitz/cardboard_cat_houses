465a1a7cb608f3f25f98423de3ef33b3382cfa2b
David Ladowitz <david@ladowitz.com>
2013-03-06 23:27:55 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CardboardCatHouses::Application.config.secret_token = '8d3c72a2156acee7492c87dd1019c483672a3267be1af31b64357f7dda2318ffaa589c7453276a4f97c5d94afa04aed3d36d682844917aa94e5c99b043a86a23'
